3eb6d069be5e2a074fea646cfb849e2ae0a07e77
David Kleinjan <dkleinja@seaquestgpvm01.fnal.gov>
2016-11-30 11:02:26 -0600

    my changes I want to keep

--- external/pythiaLumi.cxx
+++ external/pythiaLumi.cxx
@@ -9,6 +9,7 @@
 #include <TFile.h>
 #include <TTree.h>
 #include <TVector3.h>
+#include <TLorentzVector.h>
 #include <TClonesArray.h>
 
 #include "Pythia8/Pythia.h"
@@ -25,20 +26,10 @@
 using namespace std;
 using namespace Pythia8;
 
-DPSimConfig* p_config;
-
-//All tracks passed to this function are muons, and the mother
-//particles are pions
-bool keepTrack(double decayLength, double z0, int parentID, TVector3 mom)
-{
-    double costheta = mom.CosTheta();
-    return costheta < p_config->cosThetaMax && costheta > p_config->cosThetaMin;
-}
-
 int main(int argc, char* argv[])
 {
     //Initialize the configuration
-    p_config = DPSimConfig::instance();
+    DPSimConfig* p_config = DPSimConfig::instance();
     p_config->init(argv[1]);
 
     //Initialize vertex generator
@@ -49,15 +40,22 @@ int main(int argc, char* argv[])
     int eventID;
     int n;
     int pdg[10000];
+    int parentID[10000];
+    float energy[10000];
     TClonesArray* p_pos = new TClonesArray("TVector3");  p_pos->BypassStreamer(); TClonesArray& pos = *p_pos;
     TClonesArray* p_mom = new TClonesArray("TVector3");  p_mom->BypassStreamer(); TClonesArray& mom = *p_mom;
 
+    TH1I *Hcount = new TH1I("Hcount", "p+p collisions with/without particles", 2, 0, 2);
+    TH2F *Hbeam = new TH2F("Hbeam","Hbeam", 150, -1.5, 1.5, 150, -1.5, 1.5);
+    
     TFile* saveFile = new TFile(p_config->outputFileName, "recreate");
     TTree* saveTree = new TTree("save", "save");
 
     saveTree->Branch("eventID", &eventID, "eventID/I");
     saveTree->Branch("n", &n, "n/I");
     saveTree->Branch("pdg", pdg, "pdg[n]/I");
+    saveTree->Branch("parentID", parentID, "parentID[n]/I");
+    saveTree->Branch("energy", energy, "energy[n]/F");
     saveTree->Branch("pos", &p_pos, 256000, 99);
     saveTree->Branch("mom", &p_mom, 256000, 99);
 
@@ -74,11 +72,17 @@ int main(int argc, char* argv[])
     pnGen.readString("Beams:idB = 2112");
     pnGen.init();
 
+    //simple lorentzvector
+    TLorentzVector thetacheck;
     //Main loop
     int nEvents = p_config->nEvents;
     for(int i = 0; i < nEvents; ++i)
     {
-        TVector3 vtx = p_vertexGen->generateVertex();
+        double zvtx = p_vertexGen->generateVertex();
+	double xvtx = G4RandGauss::shoot(0.,0.68);
+	double yvtx = G4RandGauss::shoot(0.,0.76);
+	if(xvtx*xvtx/1.7/1.7*4+ yvtx*yvtx/1.9/1.9*4 > 1) continue;
+	Hbeam -> Fill(xvtx, yvtx);
         double pARatio = p_vertexGen->getPARatio();
 
         Pythia* p_pythia = G4UniformRand() < pARatio ? &ppGen : &pnGen;
@@ -90,28 +94,52 @@ int main(int argc, char* argv[])
         int nParticles = events.size();
         for(int j = 1; j < nParticles; ++j)
         {
-            if(events[j].status() > 0)
-            {
-                if(!keepTrack(events[j].zProd()/10., vtx.Z(), events[events[j].mother1()].id(), TVector3(events[j].px(), events[j].py(), events[j].pz()))) continue;
+          thetacheck.SetPxPyPzE(events[j].px(), events[j].py(), events[j].pz(), events[j].e());
+          if(events[j].status()<0)continue;
+	  //if(!events[j].isCharged()) continue;
+	  if(events[j].id() > 2000) continue;
+	  if(abs(events[j].py()/events[j].px()) > 0.176 && abs(events[j].py()/events[j].px()) < 5.67 )continue;//#phi = #pm 10 deg at 0, 90, 180, 360
+	  if(abs(thetacheck.CosTheta()) < 0.174 && (events[j].zProd()/10. + zvtx)< -0.)//#theta = #pm 10 deg
+	  {
 
-                //Fill muon
+                //Fill normal particles 
                 pdg[n] = events[j].id();
-                new(mom[n]) TVector3(events[j].px(), events[j].py(), events[j].pz());
-                new(pos[n]) TVector3(events[j].xProd()/10. + vtx.X(), events[j].yProd()/10. + vtx.Y(), events[j].zProd()/10. + vtx.Z());
+		parentID[n] = events[events[j].mother1()].id();
+		energy[n] = events[j].e();
+		new(mom[n]) TVector3(events[j].px(), events[j].py(), events[j].pz());
+                new(pos[n]) TVector3(events[j].xProd()/10. + xvtx, events[j].yProd()/10. + yvtx, events[j].zProd()/10. + zvtx);
+
                 ++n;
+
+                /*
+                //If mother particle exists, fill mother as well
+                int motherID = events[j].mother1();
+                if(motherID < 1 || motherID >= nParticles) continue;
+                if(abs(events[motherID].id()) < 10) continue;
+                pdg[n] = events[motherID].id();
+                new(mom[n]) TVector3(events[motherID].px(), events[motherID].py(), events[motherID].pz());
+                new(pos[n]) TVector3(events[motherID].xProd()/10., events[motherID].yProd()/10., events[motherID].zProd()/10. + zvtx);
+                ++n;*/
             }
         }
 
-        if(n == 0) continue;
-
-        saveTree->Fill();
-        mom.Clear();
-        pos.Clear();
+        if(n == 0){
+	  Hcount -> Fill(0);
+	  //continue;
+	}
+	else{
+	  Hcount -> Fill(1);
+	  saveTree->Fill();
+	  mom.Clear();
+	  pos.Clear();
+	}
     }
-
+    
     //finalize
     saveFile->cd();
     saveTree->Write();
+    Hcount->Write();
+    Hbeam->Write();
     saveFile->Close();
 
     return EXIT_SUCCESS;
